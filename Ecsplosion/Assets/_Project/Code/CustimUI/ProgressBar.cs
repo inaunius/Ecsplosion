429bb2255d2cd7dbe50b424657982afaf52ef1f3
Yury Verpatov <inaunius@gmain.com>
2025-08-13 15:26:43 +0600

    UI Created

---
+++ Ecsplosion/Assets/_Project/Code/CustimUI/ProgressBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Inaunius.Ecsplosion
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 1f)] private float _progress;
+
+        [SerializeField] private Image _fillImage;
+
+        public float Progress
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                _fillImage.transform.localScale = new Vector3(
+                    _progress,
+                    _fillImage.transform.localScale.y,
+                    _fillImage.transform.localScale.z
+                );
+            }
+        }
+
+		private void OnValidate()
+		{
+            Progress = _progress;
+		}
+	}
+}
